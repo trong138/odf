f124e8e4d596f389dbb3619387f9155b4c4fa321
trongnv <trongnv@ows.vn>
2019-03-23 12:11:49 +0700

    inti

--- actions/init.js
+++ actions/init.js
@@ -19,11 +19,8 @@ class InitAction {
     constructor(params) {
         this.globalStorage = new GlobalStorage();
         this.projectStorage = new ProjectStorage();
-        this.auth = this.globalStorage.read();
-        this.git_lab_utils = new GitLabUtils();
-        this.redmine = new Redmine(this.auth.redmine_host, {
-            apiKey: this.auth.redmine_apikey
-        });
+        // this.auth = this.globalStorage.read();
+        this.git_utils = new GitLabUtils();
     }
 
     do(resolve, reject) {
@@ -33,6 +30,67 @@ class InitAction {
         co((function* () {
             "use strict";
             var result = {};
+            var redmine_host = [
+                'https://redmine.ows.vn'
+            ];
+            var git_host = [
+                'https://git.ows.vn',
+                'https://api.github.com/'
+            ]
+
+            // select redmine_host
+            console.log("*************************************************************")
+            for (var i = 0; i < redmine_host.length; i++) {
+                console.log("Redmine %s: %s", i, redmine_host[i]);
+            }
+            console.log("****************Select redmine_host id in above*******************")
+            var redmine_host_id = 0;
+            while (true) {
+                redmine_host_id = parseInt((yield convertAsync(prompt, prompt.get, [['redmine_host_id']])).redmine_host_id);
+                if (redmine_host_id >= 0 && redmine_host_id < redmine_host.length) {
+                    break;
+                }
+            }
+            result.redmine_host = redmine_host[redmine_host_id];
+            //select git_host
+            console.log("*************************************************************")
+            for (var i = 0; i < git_host.length; i++) {
+                console.log("Git %s: %s", i, git_host[i]);
+            }
+            console.log("****************Select git_host id in above*******************")
+            var git_host_id = 0;
+            while (true) {
+                git_host_id = parseInt((yield convertAsync(prompt, prompt.get, [['git_host_id']])).git_host_id);
+                if (git_host_id >= 0 || git_host_id < redmine_host.length) {
+                    break;
+                }
+            }
+            result.git_host = git_host[git_host_id];
+            // insert redmine_apikey
+            result.redmine_apikey = "";
+            while (true) {
+                result.redmine_apikey = (yield convertAsync(prompt, prompt.get, [['redmine_apikey']])).redmine_apikey;
+                if (result.redmine_apikey == "") {
+                    continue;
+                }
+                break;
+            }
+            // insert git_token
+            result.git_token = "";
+            while (true) {
+                result.git_token = (yield convertAsync(prompt, prompt.get, [['git_token']])).git_token;
+                if (result.git_token == "") {
+                    continue;
+                }
+                break;
+            }
+
+
+            this.redmine = new Redmine(result.redmine_host, {
+                apiKey: result.redmine_apikey
+            });
+
+            // identify project
             result.redmine_project_identifier = (yield convertAsync(prompt, prompt.get, [['redmine_project_identifier']])).redmine_project_identifier;
             let me = yield this.getRedmineMe();
             let project = yield this.getProjectInfo(result.redmine_project_identifier);
@@ -51,8 +109,7 @@ class InitAction {
 
             while (true) {
                 var gitlab_project_name = (yield convertAsync(prompt, prompt.get, [['gitlab_project']])).gitlab_project;
-                console.log('ows_gitlab_project_name', gitlab_project_name);
-                var projects = yield this.git_lab_utils.searchProject(gitlab_project_name);
+                var projects = yield this.git_utils.searchProject(gitlab_project_name);
                 if (projects.length <= 0) {
                     console.log("ERROR:Cannot find any project with name:%s", gitlab_project_name);
                     continue;
@@ -66,10 +123,9 @@ class InitAction {
                 var project_id = 0;
                 while (true) {
                     project_id = parseInt((yield convertAsync(prompt, prompt.get, [['project_id']])).project_id);
-                    if (project_id < 0 || project_id >= projects.length) {
-                        continue;
+                    if (project_id >= 0 || project_id < projects.length) {
+                        break;
                     }
-                    break;
                 }
                 result.gitlab_project_id = projects[project_id].id;
                 break;
@@ -77,11 +133,6 @@ class InitAction {
             // result.chat_channel = (yield convertAsync(prompt, prompt.get, [['chat_channel']])).chat_channel;
             console.log('result-ows', result);
             this.projectStorage.write(result);
-
-            // const chat_utils = new ChatUtils();
-            var project_link = `${this.auth.redmine_host}/projects/${result.redmine_project_identifier}`;
-            // yield chat_utils.log("User inited project", `Inited project ${result.redmine_project_identifier}`, `User @${me.user.login}`, project_link);
-
             child_process.execSync('echo "\n.odf.json" >> .gitignore')
 
             return "Saved";
--- actions/login.js
+++ actions/login.js
@@ -3,26 +3,20 @@ const prompt = require('prompt');
 const Redmine = require('node-redmine');
 const convertAsync = require('convert-async');
 const co = require('co');
-const input_data = [
-    'redmine_apikey',
-    'gitlab_token'
-];
-
-const GlobalStorage = require('../storages/global')
+const ProjectStorage = require('../storages/project')
 
 class InitAction {
     constructor(params) {
-        this.globalStorage = new GlobalStorage();
+        this.ProjectStorage = new ProjectStorage();
     }
 
     do(resolve, reject) {
         console.log("Goto https://redmine.ows.vn/my/account to get your accesskey.");
         console.log("Goto https://git.ows.vn/profile/personal_access_tokens to get gitlab token");
         prompt.start();
-        prompt.get(input_data, (err, result) => {
-            console.log('result', result);
-            // result.redmine_host = 'https://redmine.ows.vn';
-            // result.gitlab_host = 'https://git.ows.vn';
+
+        co((function* () {
+            var result = {};
             var redmine_host = [
                 'https://redmine.ows.vn'
             ];
@@ -30,58 +24,73 @@ class InitAction {
                 'https://git.ows.vn'
             ]
 
-            co((function* () {
-                // select redmine_host
-                console.log("*************************************************************")
-                for (var i = 0; i < redmine_host.length; i++) {
-                    console.log("Redmine %s: %s", i, redmine_host[i]);
-                }
-                console.log("****************Select redmine_host id in above*******************")
-                var redmine_host_id = 0;
-                while (true) {
-                    redmine_host_id = parseInt((yield convertAsync(prompt, prompt.get, [['redmine_host_id']])).redmine_host_id);
-                    if (redmine_host_id < 0 || redmine_host_id >= redmine_host.length) {
-                        continue;
-                    }
+            // select redmine_host
+            console.log("*************************************************************")
+            for (var i = 0; i < redmine_host.length; i++) {
+                console.log("Redmine %s: %s", i, redmine_host[i]);
+            }
+            console.log("****************Select redmine_host id in above*******************")
+            var redmine_host_id = 0;
+            while (true) {
+                redmine_host_id = parseInt((yield convertAsync(prompt, prompt.get, [['redmine_host_id']])).redmine_host_id);
+                if (redmine_host_id >= 0 && redmine_host_id < redmine_host.length) {
                     break;
                 }
-                result.redmine_host = redmine_host[redmine_host_id];
-                //select git_host
-                console.log("*************************************************************")
-                for (var i = 0; i < git_host.length; i++) {
-                    console.log("Git %s: %s", i, git_host[i]);
-                }
-                console.log("****************Select git_host id in above*******************")
-                var git_host_id = 0;
-                while (true) {
-                    git_host_id = parseInt((yield convertAsync(prompt, prompt.get, [['git_host_id']])).git_host_id);
-                    if (git_host_id < 0 || git_host_id >= redmine_host.length) {
-                        continue;
-                    }
+            }
+            result.redmine_host = redmine_host[redmine_host_id];
+            //select git_host
+            console.log("*************************************************************")
+            for (var i = 0; i < git_host.length; i++) {
+                console.log("Git %s: %s", i, git_host[i]);
+            }
+            console.log("****************Select git_host id in above*******************")
+            var git_host_id = 0;
+            while (true) {
+                git_host_id = parseInt((yield convertAsync(prompt, prompt.get, [['git_host_id']])).git_host_id);
+                if (git_host_id >= 0 || git_host_id < redmine_host.length) {
                     break;
                 }
-                result.git_host = git_host[git_host_id];
+            }
+            result.git_host = git_host[git_host_id];
+            // insert redmine_apikey
+            result.redmine_apikey = "";
+            while (true) {
+                result.redmine_apikey = (yield convertAsync(prompt, prompt.get, [['redmine_apikey']])).redmine_apikey;
+                if (result.redmine_apikey == "") {
+                    continue;
+                }
+                break;
+            }
+            // insert git_token
+            result.git_token = "";
+            while (true) {
+                result.git_token = (yield convertAsync(prompt, prompt.get, [['git_token']])).git_token;
+                if (result.git_token == "") {
+                    continue;
+                }
+                break;
+            }
 
+            console.log('result', result);
 
-                this.redmine = new Redmine(result.redmine_host, {
-                    apiKey: result.redmine_apikey
-                });
-                "use strict";
-                let me = yield this.getRedmineMe();
-
-                result.redmine_user_id = me.user.id;
-                result.redmine_user_login = me.user.login;
-                result.redmine_user_email = me.user.email;
-
-
-                this.globalStorage.write(result);
-
-                return "Saved";
-            }).bind(this)).then(function (value) {
-                resolve(value);
-            }, function (err) {
-                reject(err);
+            this.redmine = new Redmine(result.redmine_host, {
+                apiKey: result.redmine_apikey
             });
+            "use strict";
+            let me = yield this.getRedmineMe();
+
+            result.redmine_user_id = me.user.id;
+            result.redmine_user_login = me.user.login;
+            result.redmine_user_email = me.user.email;
+
+            this.ProjectStorage.write({ login: result });
+
+            return "Saved";
+
+        }).bind(this)).then(function (value) {
+            resolve(value);
+        }, function (err) {
+            reject(err);
         });
     }
 
